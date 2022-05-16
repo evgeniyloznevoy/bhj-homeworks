37af99c843d77d781e56178ba7f306e258588c52
Evgeniy Loznevoy <evgeniy.loznevoy@yandex.ru>
2022-05-16 18:46:38 +0300

    task ready

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,21 +1,45 @@
 'use strict';
 
-const reveal = document.querySelectorAll('.reveal');
+const book = document.getElementById('book');
+const fontSizes = document.getElementsByClassName('font-size');
+const textColors = document.querySelector('.book__control_color').getElementsByClassName('color');
+const backgroundColors = document.querySelector('.book__control_background').getElementsByClassName('color');
 
-document.addEventListener('scroll', show);
+for (const size of fontSizes) 
+  size.addEventListener('click', function() {
+    event.preventDefault();
+    const oldSize = this.parentElement.querySelector('.font-size_active');
+    if (this === oldSize) 
+      return;
+    oldSize.classList.remove('font-size_active');
+    this.classList.add('font-size_active');
+    
+    book.classList.remove('book_fs-'+oldSize.dataset.size);
+    book.classList.add('book_fs-'+this.dataset.size);
+  })
+  
+for (const color of textColors) 
+  color.addEventListener('click', function() {
+    event.preventDefault();
+    const oldColor = this.parentElement.querySelector('.color_active');
+    if (this === oldColor) 
+      return;
+    oldColor.classList.remove('color_active');
+    this.classList.add('color_active');
+    
+    book.classList.remove('book_color-'+oldColor.dataset.color);
+    book.classList.add('book_color-'+this.dataset.color);
+  })
 
-
-function show(event) {
-
-    const viewportHeight = window.innerHeight;
-
-    for (let element of reveal) {
-
-        const elementTop = element.getBoundingClientRect().top;
-        if (elementTop < viewportHeight) {
-            element.classList.add('reveal_active');
-        } else {
-            element.classList.remove('reveal_active');
-        }
-    }
-}
+for (const color of backgroundColors) 
+  color.addEventListener('click', function() {
+    event.preventDefault();
+    const oldColor = this.parentElement.querySelector('.color_active');
+    if (this === oldColor) 
+      return;
+    oldColor.classList.remove('color_active');
+    this.classList.add('color_active'); 
+    
+    book.classList.remove('book_bg-'+oldColor.dataset.color);
+    book.classList.add('book_bg-'+this.dataset.color);
+  })
