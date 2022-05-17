b9751ae571b1bc0799822fc9b4b04f525a395b44
Evgeniy Loznevoy <evgeniy.loznevoy@yandex.ru>
2022-05-17 12:19:40 +0300

    task ready

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,45 +1,27 @@
 'use strict';
 
-const book = document.getElementById('book');
-const fontSizes = document.getElementsByClassName('font-size');
-const textColors = document.querySelector('.book__control_color').getElementsByClassName('color');
-const backgroundColors = document.querySelector('.book__control_background').getElementsByClassName('color');
+const reader = document.querySelector('.book');
+const controls = document.querySelector('.book__control');
 
-for (const size of fontSizes) 
-  size.addEventListener('click', function() {
-    event.preventDefault();
-    const oldSize = this.parentElement.querySelector('.font-size_active');
-    if (this === oldSize) 
-      return;
-    oldSize.classList.remove('font-size_active');
-    this.classList.add('font-size_active');
-    
-    book.classList.remove('book_fs-'+oldSize.dataset.size);
-    book.classList.add('book_fs-'+this.dataset.size);
-  })
-  
-for (const color of textColors) 
-  color.addEventListener('click', function() {
-    event.preventDefault();
-    const oldColor = this.parentElement.querySelector('.color_active');
-    if (this === oldColor) 
-      return;
-    oldColor.classList.remove('color_active');
-    this.classList.add('color_active');
-    
-    book.classList.remove('book_color-'+oldColor.dataset.color);
-    book.classList.add('book_color-'+this.dataset.color);
-  })
+for (let control of controls.children) {
+    control.addEventListener('click', change)
+}
 
-for (const color of backgroundColors) 
-  color.addEventListener('click', function() {
+function change(event) {
     event.preventDefault();
-    const oldColor = this.parentElement.querySelector('.color_active');
-    if (this === oldColor) 
-      return;
-    oldColor.classList.remove('color_active');
-    this.classList.add('color_active'); 
-    
-    book.classList.remove('book_bg-'+oldColor.dataset.color);
-    book.classList.add('book_bg-'+this.dataset.color);
-  })
+    if (event.target.dataset.size === 'big') {
+        reader.classList.add('book_fs-big');
+        reader.classList.remove('book_fs-small');
+    } else if (event.target.dataset.size === 'small') {
+        reader.classList.add('book_fs-small');
+        reader.classList.remove('book_fs-big');
+    } else {
+        reader.classList.remove('book_fs-small');
+        reader.classList.remove('book_fs-big');
+    }
+
+    for (let letter of document.querySelectorAll('.font-size')) {
+        letter.classList.remove('font-size_active');
+    }
+    event.target.classList.add('font-size_active');
+}
